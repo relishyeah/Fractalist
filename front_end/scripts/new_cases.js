3e198f7d01623dbe9e1c31cd7f17db4277e37395
Spencer Vilicic <quence@bu.edu>
2021-04-23 12:30:02 -0400

    update for merge

--- front_end/scripts/new_cases.js
+++ front_end/scripts/new_cases.js
@@ -26,34 +26,64 @@ switch (caseNum) {
         redNum = 0;
         greenNum = Math.floor(256 / rgbIncrements) * remainNum;
         blueNum = 255;
+        if (redNum == 0 && greenNum == 0) {
+          redNum = 255;
+          greenNum = 255;
+        }
       break;
     case 3:
         //Major mode, low valence (< 0.5), low energy (< 0.5)
       redNum = Math.floor(256 / rgbIncrements) * remainNum;
       greenNum = 0;
       blueNum = 127;
+      if (redNum == 0 && greenNum == 0 && blueNum == 0) {
+        redNum = 255;
+        greenNum = 255;
+        blueNum = 255;
+      }
       break;
     case 4:
         //minor mode, high valence (>= 0.5), high energy (>= 0.5)
       redNum = 255;
       greenNum = Math.floor(256 / rgbIncrements) * remainNum;
       blueNum = 0;
+      if (redNum == 0 && greenNum == 0 && blueNum == 0) {
+        redNum = 255;
+        greenNum = 255;
+        blueNum = 255;
+      }
       break;
     case 5:
         //minor mode, high valence (>= 0.5), low energy (< 0.5)
       redNum = 127;
       greenNum = Math.floor(256 / rgbIncrements) * remainNum;
       blueNum = 0;
+      if (redNum == 0 && greenNum == 0 && blueNum == 0) {
+        redNum = 255;
+        greenNum = 255;
+        blueNum = 255;
+      }
       break;
     case 6:
         //minor mode, low valence (< 0.5), high energy (>= 0.5)
       redNum = 0;
       greenNum = Math.floor(256 / rgbIncrements) * remainNum;
       blueNum = 255;
+      if (redNum == 0 && greenNum == 0 && blueNum == 0) {
+        redNum = 255;
+        greenNum = 255;
+        blueNum = 255;
+      }
+      break;
     case 7:
         //minor mode, low valence (< 0.5), low energy (< 0.5)
       redNum = 0;
       greenNum = Math.floor(256 / rgbIncrements) * remainNum;
       blueNum = 127;
+      if (redNum == 0 && greenNum == 0 && blueNum == 0) {
+        redNum = 255;
+        greenNum = 255;
+        blueNum = 255;
+      }
     break;
   }
